0ede0b74134036fa7cfffd6ff5e1cea5e634f463
Lennart Meyer <lennart.meyer@exxeta.com>
2026-01-28 21:37:15 +0100

    SNAPP-4: remove unused import

--- composeApp/src/commonMain/kotlin/de/mkrabs/snablo/app/presentation/ui/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/de/mkrabs/snablo/app/presentation/ui/HomeScreen.kt
@@ -1,10 +1,7 @@
 package de.mkrabs.snablo.app.presentation.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.DrawerValue
